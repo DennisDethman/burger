83b158aa563017c601debd72a9241c5e0a0c056b
Dennis <dwptwo@gmail.com>
2018-08-28 20:11:38 -0600

    connection.js.. removed commented-out connections

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,5 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-//
-//
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password",
-//     database: "burgers_db"
-// });
-
 
 
 var connection;
